c5935246798ac707d603cf31eba0e2014537ed84
nicolads <111922549+noelzada@users.noreply.github.com>
2024-03-22 10:07:12 -0300

    assdsa

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
+
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-08-18T15:37:07")
+const tempoObjetivo1 = new Date("2024-06-15T00:00:00");
+let tempoAtual = new Date();
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 
-let tempo = tempoObjetivo1.getFullYear();
 
-contadores[0].textContent = tempo;
+
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
+}
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 
 let segundos = Math.floor(tempoFinal / 1000);
 let minutos = Math.floor(segundos / 60);
