3ee28c1f5e6e04f976edd8de24be26c12bee1612
nicolads <111922549+noelzada@users.noreply.github.com>
2024-03-22 10:01:41 -0300

    asds

--- main.js
+++ main.js
@@ -4,3 +4,8 @@ const tempoObjetivo1 = new Date("2023-08-18T15:37:07")
 let tempo = tempoObjetivo1.getFullYear();
 
 contadores[0].textContent = tempo;
+
+let segundos = Math.floor(tempoFinal / 1000);
+let minutos = Math.floor(segundos / 60);
+let horas = Math.floor(minutos / 60);
+let dias = Math.floor(horas / 24);
